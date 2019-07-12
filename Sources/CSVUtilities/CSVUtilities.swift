f1bc9e77b5f9a5ea6b5cb1bf5c01834b7991098d
Jake Young <jwebby@me.com>
2019-07-12 16:55:37 -0400

    Stubs in base protocol and helper methods for arrays

--- Sources/CSVUtilities/CSVUtilities.swift
+++ Sources/CSVUtilities/CSVUtilities.swift
@@ -1,3 +1,19 @@
-struct CSVUtilities {
-    var text = "Hello, World!"
+public protocol CSVRowRepresentable {
+    static func headers -> [String]
+    static func columnValues -> [String]
+}
+
+extension Array where Element: CSVRowRepresentable {
+    
+    public func header() -> [String] {
+        return Element.headers().joined(separator: ",")
+    }
+    
+    public func rows() -> [String] {
+        return self.map { $0.columnValues().joined(separator: ",") }
+    }
+    
+    public func csv() -> String {
+        return header() + "\r" + rows().joined(separator: "\r")
+    }
 }
